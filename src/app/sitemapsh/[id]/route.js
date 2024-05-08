6a53cbc98edebf335952546c616a3707abd85ae1
sh20raj <shaswatraj3@gmail.com>
2024-05-08 20:52:18 +0530

    sitemap working

--- src/app/sitemapsh/[id]/route.js
+++ src/app/sitemapsh/[id]/route.js
@@ -1,6 +1,5 @@
 // import fetch from 'isomorphic-fetch'; // Import fetch
 
-import getImage from "@/app/functions/getImage";
 
 // import { getimagesFromDevTo } from 'someFunctions'; // Replace with your actual function to fetch images
 function generateRandomNumber(min, max) {
@@ -12,13 +11,9 @@ function generateRandomNumber(min, max) {
   
   
   // Mock function to fetch images (replace this with your actual implementation)c
-  let getdata = async (params,limit) => {
-    const response = await fetch(
-      `https://dev.to/api/images/latest/?per_page=${limit}&page=${
-        params
-      }`
-    );
-    const data = await response.json();
+  let getData = async (page = 1,limit) => {
+    let data = await fetch("https://civitai.com/api/v1/images?page="+page);
+    data = await data.json();
     return data;
   };
   
@@ -31,19 +26,23 @@ function generateRandomNumber(min, max) {
         let id = url.searchParams.has("limit") ? url.searchParams.get("limit") : 1000;
         
         console.log("id",id);
+
         let images = await getData()
+
+        images = images.items
+        console.log("images",images[0]);
   
         // Start building the XML
         let xml = '<?xml version="1.0" encoding="UTF-8"?>';
         xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
   
-        // Add each article URL to the sitemap
-        images.forEach((article) => {
+        // Add each image URL to the sitemap
+        images.forEach((image) => {
           xml += "<url>";
-          xml += `<loc>${url.origin + article.path}</loc>`; // Modify URL structure as needed
-          xml += `<lastmod>${new Date(
-            article.published_at
-          ).toISOString()}</lastmod>`; // Use published date as last modified
+          xml += `<loc>${url.origin +"/"+ image.id}</loc>`; // Modify URL structure as needed
+        //   xml += `<lastmod>${new Date(
+        //     image.published_at
+        //   ).toISOString()}</lastmod>`; // Use published date as last modified
           xml += "<changefreq>daily</changefreq>"; // You can adjust the change frequency
           xml += "<priority>1</priority>"; // Priority can be adjusted based on the importance of the page
           xml += "</url>";
