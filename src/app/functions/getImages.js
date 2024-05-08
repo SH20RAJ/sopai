38b0da6416f9db419f526ff71144cb84555f02c0
sh20raj <shaswatraj3@gmail.com>
2024-05-08 17:33:42 +0530

    SopAI

--- src/app/functions/getImages.js
+++ src/app/functions/getImages.js
@@ -1,7 +1,7 @@
 
 export default async function getImages() {
 
-    let data = await fetch("https://SopAI.com/api/v1/images");
+    let data = await fetch("https://civitai.com/api/v1/images");
     data = await data.json();
     return data;
 }
